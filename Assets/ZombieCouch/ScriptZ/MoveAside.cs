3339a0c1025bdd54903a407f96bbd800fa0049d6
Arshd <arshad@sort6.com>
2018-01-27 22:58:24 +0200

    zombie Hands prfabs & lose condition

--- Assets/ZombieCouch/ScriptZ/MoveAside.cs
+++ Assets/ZombieCouch/ScriptZ/MoveAside.cs
@@ -6,6 +6,13 @@ public class MoveAside : MonoBehaviour {
 
 	void OnMouseDrag()
 	{
-		transform.RotateAround(transform.parent.position, new Vector3(0f, 0f, 1f), amIRight ? 20f : -20f);
+		//transform.RotateAround(transform.parent.position, new Vector3(0f, 0f, 1f), amIRight ? 20f : -20f);
+
+		if (Quaternion.Angle(transform.rotation, amIRight ? Quaternion.Euler(0f, 0f, 170f) :  Quaternion.Euler(0f, 0f, -170f)) >= 3f)// Quaternion.Euler(0f, 0f, 30f))
+		{
+			transform.parent.RotateAround(transform.parent.position, new Vector3(0f, 0f, 1f), amIRight ? 5f : -5f);//Random.Range(0.5f, 1.5f));
+
+			transform.parent.parent.GetComponent<RotateHands>().lft = transform.parent.parent.GetComponent<RotateHands>().rit = false;
+		}
 	}
 }
