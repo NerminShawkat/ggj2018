81e86d26130b300522d53ff91cbc02760b1ed42a
Arshd <arshad@sort6.com>
2018-01-28 12:56:25 +0200

    clear

--- Assets/ZombieCouch/ScriptZ/LookTowardZ.cs
+++ Assets/ZombieCouch/ScriptZ/LookTowardZ.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
+﻿﻿using UnityEngine;
 
 public class LookTowardZ : MonoBehaviour {
 	
@@ -8,14 +7,3 @@ public class LookTowardZ : MonoBehaviour {
 		transform.LookAt(Camera.main.transform);
 	}
 }
-=======
-﻿using UnityEngine;
-
-public class LookTowardZ : MonoBehaviour {
-	
-	// Update is called once per frame
-	void Update () {
-		transform.LookAt(Camera.main.transform);
-	}
-}
->>>>>>> 2ca247e5dd668d6cbb6bc5530b08f455cf55befe
