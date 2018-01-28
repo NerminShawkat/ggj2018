b1f6a3abec6c337c4dcdadf050ef3b59a3d0e5d0
Arshd <arshad@sort6.com>
2018-01-28 18:43:00 +0200

    duh

--- Assets/Loading.cs
+++ Assets/Loading.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Loading : MonoBehaviour {
 
+	public string[] news;
+
+	public Sprite[] newsHeadPicture;
+
+	public Text textHead;
+	public SpriteRenderer newsPix;
+
+
 	IEnumerator Start()
     {
+		int levelNum = PlayerPrefs.GetInt("level");
+		textHead.text = news[levelNum - 2];
+		newsPix.sprite = newsHeadPicture[levelNum - 2];
         yield return new WaitForSeconds(3);
-        int levelNum = PlayerPrefs.GetInt("level");
         SceneManager.LoadScene(levelNum);
     }
 }
